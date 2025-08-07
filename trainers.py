15544de9ec9ae131a4cd8935de81f947b4136f97
Danial <Danielparnian2@gmail.com>
2025-08-07 13:18:39 +0330

    METHOD1: arxiv cocovin trainer debug

--- trainers.py
+++ trainers.py
@@ -491,36 +491,41 @@ class CoCoVinTrainer(BaseTrainer):
             x_data = self.g.x.to(self.info_dict['device'])
             ori_edge_index = self.ori_edge_index.to(self.info_dict['device'])
 
-            # Forward passes for CoCoS
-            ori_logits = self.model(x_data, ori_edge_index)
+            # Get hidden representations (not final logits)
+            ori_hidden = self.model.get_embeddings(x_data, ori_edge_index)  # Use embeddings method
             shuf_feat = self.shuffle_feat(x_data)
-            shuf_logits = self.model(shuf_feat, ori_edge_index)
-            tp_shuf_nids = self.shuffle_nids()
-            tp_shuf_logits = shuf_logits[tp_shuf_nids]
+            shuf_hidden = self.model.get_embeddings(shuf_feat, ori_edge_index)
 
-            # Classification loss
-            # Add classification mode support like original CoCoS
+            # Get logits for classification
+            ori_logits = self.model.classifier(ori_hidden)  # Apply final layer
+            shuf_logits = self.model.classifier(shuf_hidden)
+
+            tp_shuf_nids = self.shuffle_nids()
+            tp_shuf_hidden = shuf_hidden[tp_shuf_nids]
+
+            # Classification loss (use logits)
             if self.info_dict['cocos_cls_mode'] == 'shuf':
                 epoch_cls_loss = self.crs_entropy_fn(shuf_logits[cls_nids], cls_labels)
             elif self.info_dict['cocos_cls_mode'] == 'raw':
                 epoch_cls_loss = self.crs_entropy_fn(ori_logits[cls_nids], cls_labels)
             elif self.info_dict['cocos_cls_mode'] == 'both':
                 epoch_cls_loss = 0.5 * (self.crs_entropy_fn(ori_logits[cls_nids], cls_labels) +
-                                        self.crs_entropy_fn(shuf_logits[cls_nids], cls_labels))
+                                      self.crs_entropy_fn(shuf_logits[cls_nids], cls_labels))
             else:
-                raise ValueError("Unexpected cls_mode parameter: {}".format(self.info_dict['cls_mode']))
+                epoch_cls_loss = self.crs_entropy_fn(ori_logits[cls_nids], cls_labels)
+
             _, preds = torch.max(ori_logits[cls_nids], dim=1)
 
-            # CoCoS Contrastive Loss
-            pos_score_f = self.Dis(torch.cat((shuf_logits, ori_logits), dim=-1))
+            # CoCoS Contrastive Loss (use hidden representations - dimension 256)
+            pos_score_f = self.Dis(torch.cat((shuf_hidden, ori_hidden), dim=-1))
             pos_loss_f = self.bce_fn(pos_score_f[ctr_nids], ctr_labels_pos)
-            pos_score_s = self.Dis(torch.cat((tp_shuf_logits, shuf_logits), dim=-1))
+            pos_score_s = self.Dis(torch.cat((tp_shuf_hidden, shuf_hidden), dim=-1))
             pos_loss_s = self.bce_fn(pos_score_s[ctr_nids], ctr_labels_pos)
             epoch_ctr_loss_pos = (pos_loss_f + pos_loss_s) / 2.0
 
             neg_nids = self.gen_neg_nids()
-            neg_ori_logits = ori_logits[neg_nids].detach()
-            neg_score = self.Dis(torch.cat((ori_logits, neg_ori_logits), dim=-1))
+            neg_ori_hidden = ori_hidden[neg_nids].detach()
+            neg_score = self.Dis(torch.cat((ori_hidden, neg_ori_hidden), dim=-1))
             epoch_ctr_loss_neg = self.bce_fn(neg_score[ctr_nids], ctr_labels_neg)
             epoch_ctr_loss = epoch_ctr_loss_pos + epoch_ctr_loss_neg
 
